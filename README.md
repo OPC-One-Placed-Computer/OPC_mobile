688c956e9aba13f5bd1d66621483d7c2b20e32b0
Rocky M. Pabalate <rockypabalate@gmail.com>
2024-06-18 14:52:27 +0800

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# opc_mobile_development
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/opc_mobile_development/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.opc_mobile_development
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
